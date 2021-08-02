a1ba4f6154d559f1f90cf0b713434d5d18878096
carlyguzylak <cguzylak@uci.edu>
2021-08-02 11:04:59 -0700

    Fixed layout of nav bar and added surprise hello functionality

---
+++ javascript.js
@@ -0,0 +1,10 @@
+
+
+
+document.getElementById("name").onclick = function(){
+
+    document.getElementById("welcome").style.display = 'flex';
+    setTimeout(function(){
+        document.getElementById("welcome").style.display = 'none';
+    }, 1600);
+}
