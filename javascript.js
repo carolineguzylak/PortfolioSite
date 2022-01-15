289a7acf1be595fd0b3ad5f59a1e8c2c835aa248
carlyguzylak <74155787+carolineguzylak@users.noreply.github.com>
2022-01-14 23:19:37 -0800

    Added bootstrap to navbar and intro header... still doens't look right in mobile orientation. Also other small adjustments

--- javascript.js
+++ javascript.js
@@ -1,8 +1,5 @@
 
-
-
 document.getElementById("name").onclick = function(){
-
     document.getElementById("welcome").style.display = 'flex';
     setTimeout(function(){
         document.getElementById("welcome").style.display = 'none';
